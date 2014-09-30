e28b9307e303e8bf8f3581dec4ca1161b98d17a8
scottbateman <sbateman@upei.ca>
2014-09-30 02:22:47 -0300

    updated main.py to be fully functional, based on curses

--- lib/main.py
+++ lib/main.py
@@ -1,221 +1,239 @@
-import RPi.GPIO as GPIO
+import curses
 import time
-import signal
-import sys
+import urllib
+import os
+import time
+import threading
 import json
-import urllib2
-import traceback
+from subprocess import call, Popen
 
-DEBUG = True		#print debugging messages
-TEST = True		#run in testing mode
+DEVNULL = open(os.devnull,'wb') #represent null device, to write output to
+RED = 1 
+GREEN = 2 
+ORANGE = 3
+BLACK = 4
+
+DEBUG = False       #print debugging messages
+TEST = False        #run in testing mode
 TEST_COUNTER = 0
-
-RED_LED = 8		#the pin number of the RED LED
-GREEN_LED = 7		#the pin number of the GREEN LED
+USE_CURSES = True   #display the curses interface, may be useful to set false for debugging 
 
 #the number of seconds to sleep between scraping data
-SLEEP_TIME_TESTING = 10 #the sleep time when in testing mode
-SLEEP_TIME_NORMAL = 300 #the sleep time when in normal mode
+SLEEP_TIME_TESTING = 10   #the sleep time when in testing mode
+SLEEP_TIME_NORMAL = 60   #the sleep time when in normal mode
 
-if TEST:
-    SLEEP_TIME = SLEEP_TIME_TESTING 
-else:
-    SLEEP_TIME = SLEEP_TIME_NORMAL 
+SLEEP_TIME = SLEEP_TIME_TESTING if TEST else SLEEP_TIME_NORMAL 
 
 #the url to scrape energy data from
 ENERGY_URL = "http://www.gov.pe.ca/energy/js/chart-values.php"
+CHART_URL1 = "https://api.cosm.com/v2/feeds/64374/datastreams/on-island-load.png?width=1000&height=250&colour=%23f15a24&duration=1day&show_axis_labels=true&detailed_grid=true&scale=manual&min=0&max=220&timezone=Atlantic%20Time%20(Canada)"
+CHART_URL2 = "https://api.cosm.com/v2/feeds/64374/datastreams/percentage-wind.png?width=1000&height=250&colour=%23f15a24&duration=1day&show_axis_labels=true&detailed_grid=true&scale=manual&min=0&max=150&timezone=Atlantic%20Time%20(Canada)"
+CHARTS = [CHART_URL1, CHART_URL2]
 
+#energy values
 MAX_LOAD = 230          #we are approaching the max load of the island at 230 MW
 WIND_HIGH_EXPORT = 70   #a high level of wind is being exported at 70MW
 WIND_HIGH_LEVEL = 100   #we are producing a high amount of wind at 100MW
 WIND_MIN_LEVEL = 10     #we are producing a minimal amount of wind at 10MW
 SAMPLE_DATA = {'on-island-wind':113.03, 'wind-export': 50, 'on-island-fossil': 0, 'updateDate':13931464841, 'on-island-load': 157.49, 'error': 0, 'wind-local':63.82}
 
-GPIO.setwarnings(False)
+#setup curses
+if USE_CURSES: 
+  stdscr = curses.initscr()
+  curses.start_color()
+  curses.cbreak()
+  curses.noecho()
+  stdscr.keypad(1)
+  curses.mousemask(1)
 
-#setup and continuously display data until something goes wrong
-def main():
-    global TEST
-    global DEBUG
-    global TEST_COUNTER
+  #setup colors
+  curses.init_pair(BLACK, curses.COLOR_WHITE, curses.COLOR_BLACK)
+  curses.init_pair(RED, curses.COLOR_WHITE, curses.COLOR_RED)
+  curses.init_pair(GREEN, curses.COLOR_WHITE, curses.COLOR_GREEN) 
+  curses.init_pair(ORANGE, curses.COLOR_WHITE, curses.COLOR_YELLOW) 
+  curses.init_color(ORANGE,1000,900,0)
 
-    print "-----------------------------------------"
-    print "pei-energy-feedback-thingy is now running"
-    print "press Ctrl-C to quit."
-    print "-----------------------------------------"
+def fill_bg(color_pair):
+  '''fill the background with a particular color, takes a pair for bg and fg'''
+  if USE_CURSES:
+    global stdscr
+    height,width = stdscr.getmaxyx()
+
+    for x in range(width-2):
+      for y in range(height-2):
+        stdscr.addch(y+1,x+1,' ', curses.color_pair(color_pair))
+
+    stdscr.refresh()
+
+
+def scr_write(msg,color_pair):
+  '''write a message to the middle of the screen'''
+  if USE_CURSES:
+    global stdscr
+    height,width = stdscr.getmaxyx()
+    mid_y = height/2
+    mid_x = width/2
+
+    stdscr.addstr(mid_y,mid_x,msg, curses.color_pair(color_pair))
+    stdscr.refresh()
+  else:
+    print msg
+
+class FBIThread(threading.Thread):
+  '''thread for controlling displaying an image'''
+  def __init__(self):
+    self.fbi = None
+    if DEBUG: scr_write('create FPI thread',GREEN);
+    threading.Thread.__init__(self)
+  def run(self):
+    self.fbi = Popen('fbi -t 5 -a images/*.png',shell=True, stdout=DEVNULL, stderr=DEVNULL)
+  def terminate(self):
+    self.fbi.kill()
+    Popen('killall fbi',shell=True, stdout=DEVNULL, stderr=DEVNULL)
+
+class DataLoader(threading.Thread):
+  def __init__(self):
+    self.loading = True
+    self.on_island_load = ''
+    self.on_island_wind = ''
+    self.on_island_fossil = ''
+    self.wind_local = ''
+    self.wind_export = ''
+    threading.Thread.__init__(self)
     
-    # to use BCM pin numbers
-    GPIO.setmode(GPIO.BCM)
-    
-    # set up GPIO output channel
-    GPIO.setup(RED_LED, GPIO.OUT)
-    GPIO.setup(GREEN_LED, GPIO.OUT)
-    
-    GPIO.output(RED_LED, GPIO.LOW)
-    GPIO.output(GREEN_LED, GPIO.LOW)
-    
-    #if an interupt is received (e.g., Ctrl+C) then exit gracefully
-    signal.signal(signal.SIGINT, sig_int_handler)
-    
-    #continuously display data through the LEDs
-    while True:
-        try:
-	    if TEST:
-		data = get_sample_data(TEST_COUNTER)
-	        TEST_COUNTER += 1
-		if TEST_COUNTER >= 6:
-		    print "ending tests. returing to live mode."
-		    TEST = False
-	    else:
-		data = get_govpeca_data()
-
-	    if DEBUG:
-		print data
-
-            display_data(data)
-
-        except (urllib2.URLError,urllib2.HTTPError,ValueError):
-            print "Could not get or display data"
-             
-	    if DEBUG:
-		traceback.print_exc()
-
-            #flash both leds and try again after the sleep time
-            display_alternating_leds(RED_LED, GREEN_LED, SLEEP_TIME)
-
-#scrape energy data from the Web
-def get_govpeca_data():
+  def run(self):
     #open the url and read the response
-    req = urllib2.Request(ENERGY_URL)
-    opener = urllib2.build_opener()
-    f = opener.open(req)
-    
-    #parse the response as JSON...
-    #keep only the first element of the array
-    data = json.load(f)[0]
-    
-    #give the dictionary more meaninful names
-    data['on-island-load'] = data.pop('data1')
-    data['on-island-wind'] = data.pop('data2')
-    data['on-island-fossil'] = data.pop('data3')
-    data['wind-local'] = data.pop('data4')
-    data['wind-export'] = data.pop('data5')
-    
-    return data
+    energy_json_file = urllib.urlopen(ENERGY_URL).read()
+    #opener = urllib.build_opener()
+    #energy_json_file = opener.open(energy_req).read()
 
-def get_sample_data(count):
-    return_val = SAMPLE_DATA.copy()
+    #loop while thread alive
+    while self.loading:   
+      if DEBUG: scr_write('starting run',GREEN)
+      try:
+        #parse the response as JSON...
+        #keep only the first element of the array
+        tempdata = json.loads(energy_json_file)[0]
 
-    if count == 0:
-        #make max_load
-	print "Approaching max provicinal load."
-        return_val['on-island-load'] = MAX_LOAD + 1	
-    elif count == 1:
-        #make fossil fuel
-	print "Burning fossil fuel."
-	return_val['on-island-fossil'] = 10
-    elif count == 2:
- 	#no wind 	
-	print "Very little to no wind power being generated."
-	return_val['on-island-wind'] = WIND_MIN_LEVEL - 1
-    elif count == 3:
-        print "generating wind energy"
-    elif count == 4: 
-        #lots of wind
-	print "Generating a good amount of wind."
-        return_val['on-island-wind'] = WIND_HIGH_LEVEL + 10
-    else:
-        #lots of wind
-	print "Exporting a high level of wind."
-        return_val['wind-export'] = WIND_HIGH_EXPORT + 10
-   
+        #give the dictionary more meaninful names
+        self.on_island_load = tempdata.pop('data1')
+        self.on_island_wind = tempdata.pop('data2')
+        self.on_island_fossil = tempdata.pop('data3')
+        self.wind_local = tempdata.pop('data4')
+        self.wind_export = tempdata.pop('data5')
+        #if DEBUG: scr_write(self.__str__(),GREEN);
 
-    if (DEBUG):
-        print "sample data: "+str(SAMPLE_DATA)
-      
-    return return_val
+        #download chart images
+        for i,chart in enumerate(CHARTS):
+          urllib.urlretrieve(chart,'images/chart'+str(i)+'.png')
 
-#display the data using the LEDs
-def display_data(data):
-    
+        if DEBUG: scr_write('done image',GREEN)
+
+        color, flash_speed = self.display_data()
+        fill_bg(color)
+        time.sleep(SLEEP_TIME)
+
+      except Exception, e:
+        scr_write("Problem downloading data: %s" % e,GREEN)
+        #raise Exception
+        continue 
+
+  def terminate(self):
+    self.loading = False
+
+  def display_data(self):
     #if PEI is over the max load
-    if data['on-island-load'] >= MAX_LOAD:
-        fast_blink_led(RED_LED, SLEEP_TIME)
+    if self.on_island_load >= MAX_LOAD:
+      return RED, .5
+      #fast_blink_led(RED_LED, SLEEP_TIME)
     
     #if there is any fossil fuel in use flash slow red
-    elif data['on-island-fossil'] > 0:
-        slow_blink_led(RED_LED, SLEEP_TIME)
+    elif self.on_island_fossil > 0:
+      return RED, 1
+      #slow_blink_led(RED_LED, SLEEP_TIME)
     
     #if we are generating no or a very small amount from wind, show solid red
-    elif data['on-island-wind'] < WIND_MIN_LEVEL:
-        turn_on_led(RED_LED, SLEEP_TIME)
+    elif self.on_island_wind < WIND_MIN_LEVEL:
+      return RED, 0
+      #turn_on_led(RED_LED, SLEEP_TIME)
     
     #if we are exporting a lot of wind, flash fast green
-    elif data['wind-export'] > WIND_HIGH_EXPORT:
-        fast_blink_led(GREEN_LED, SLEEP_TIME)
+    elif self.wind_export > WIND_HIGH_EXPORT:
+      return GREEN, .5
+      #fast_blink_led(GREEN_LED, SLEEP_TIME)
     
     #if we are generating a high amount of wind
-    elif data['wind-export'] >= WIND_HIGH_LEVEL:
-        slow_blink_led(GREEN_LED, SLEEP_TIME)
+    elif self.wind_export >= WIND_HIGH_LEVEL:
+      return GREEN, 1
+      #slow_blink_led(GREEN_LED, SLEEP_TIME)
     
     #if we are generating a above a minimal amount of wind
-    elif data['wind-export'] >= WIND_MIN_LEVEL:
-        turn_on_led(GREEN_LED, SLEEP_TIME)
+    elif self.wind_export >= WIND_MIN_LEVEL:
+      return GREEN, 0
+      #turn_on_led(GREEN_LED, SLEEP_TIME)
     
     #should not be able to get here
     else:
-        turn_on_led(GREEN_LED, SLEEP_TIME)
+      #turn_on_led(ORANGE, SLEEP_TIME)
+      return ORANGE, 0
+
+  def __str__(self):
+    ret_str = ''
+    ret_str +="on_island_load: "+ str(self.on_island_load)
+    ret_str +=", on_island_wind: "+ str(self.on_island_wind)
+    ret_str +=", on_island_fossil: "+ str(self.on_island_fossil)
+    ret_str +=", wind_local: "+ str(self.wind_local)
+    ret_str +=", wind_export: "+ str(self.wind_export)
     
-    return
-
-# turn on an LED for a specified amount of seconds
-def turn_on_led(pin, seconds):
-    GPIO.output(pin,GPIO.HIGH)
-    time.sleep(seconds)
-    GPIO.output(pin,GPIO.LOW)
-    
-    return
-
-# slow blinking function -> 1 second on, 1 second off
-def slow_blink_led(pin,seconds):
-    count = 0
-    while count < seconds:
-        GPIO.output(pin,GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(pin,GPIO.LOW)
-        time.sleep(1)
-        count += 2
-    
-    return
-
-# fast blinking function -> .5 seconds on, .5 seconds off
-def fast_blink_led(pin,seconds):
-    count = 0
-    while count < seconds:
-        GPIO.output(pin,GPIO.HIGH)
-        time.sleep(.5)
-        GPIO.output(pin,GPIO.LOW)
-        time.sleep(.5)
-        count += 1
-    
-    return
-
-def display_alternating_leds(pin1, pin2, seconds):
-    count = 0
-    while count < seconds:
-        fast_blink_led(pin1, 1)
-        fast_blink_led(pin2, 1)
-        count += 2
-    
-    return
-
-
-#handle an interupt
-def sig_int_handler(signum, frame):
-    GPIO.output(RED_LED, GPIO.LOW)
-    GPIO.output(GREEN_LED, GPIO.LOW)
-    GPIO.cleanup() 
-    sys.exit(0)    
+    return ret_str
 
 if __name__ == "__main__":
-    main()
+  try:
+    fill_bg(ORANGE)
+    count = 0
+    #start thread for reading data and images
+    data_thread = DataLoader()
+    data_thread.start()
+    
+    if USE_CURSES:
+      while True:
+        event = stdscr.getch() 
+
+        #quit program on 'q'
+        if event == ord("q"): 
+          data_thread.terminate()
+          break
+
+        #on mouse press/screen touch display images for 
+        if event == curses.KEY_MOUSE:
+          if USE_CURSES:
+            _, mx, my, _, _ = curses.getmouse()
+
+          fbi = FBIThread()
+          fbi.start()
+          time.sleep(10)  
+          fbi.terminate()
+          fbi.join()
+
+        else:
+          if USE_CURSES:
+            #stdscr.addstr(mid_y,mid_x,str(event) +'--' +str(curses.KEY_MOUSE), curses.color_pair(1))
+            if DEBUG: 
+              scr_write(str(event),RED)
+            #stdscr.refresh()
+
+    else:
+      char = ''
+      while char != 'q':
+        if char == 'd':
+          print(data_thread)
+        char = raw_input()
+      print 'shutting down... please wait'
+      data_thread.terminate()
+
+  finally:
+    if USE_CURSES:
+      curses.nocbreak()
+      stdscr.keypad(0)
+      curses.echo()
+      curses.endwin()
